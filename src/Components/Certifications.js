f48b3f0684413897fffb1f16da31dea20d7fe9d5
Shashank Redd Sunkara <SunkaraShashankRedd@JohnDeere.com>
2019-11-03 19:04:06 -0500

    Add src dir

---
+++ src/Components/Certifications.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAward } from "@fortawesome/free-solid-svg-icons";
+
+
+class Certifications extends Component {
+    constructor(props) {
+        super(props);
+
+        this.certifications = props.certifications;
+    }
+
+    render() {
+        return(
+            <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="certifications">
+            <div className="w-100">
+            <h2 className="mb-5">Certifications</h2>
+            <ul className="fa-ul mb-0">
+            {
+                this.awards.map((data, index) => (
+                    <li key={index}>
+                <FontAwesomeIcon icon={faAward} color="#ffc107" />
+                <span className="ml-2"> {data.certDetails} </span>
+                </li>
+    ))
+    }
+    </ul>
+        </div>
+        </section>
+    );
+    }
+}
+
+export default Certifications;
